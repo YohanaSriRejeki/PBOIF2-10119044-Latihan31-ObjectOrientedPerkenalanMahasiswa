d4f23861e7d60cdec26046d8ac79e3931b30711f
YohanaSriRejeki <73046650+YohanaSriRejeki@users.noreply.github.com>
2020-11-06 19:38:21 +0700

    Add files via upload

---
+++ src/pboif2/pkg10119044/latihan31/objectorientedperkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119044.latihan31.objectorientedperkenalanmahasiswa;
+
+/**
+ *
+ * @author 
+ * NAMA     : Yohana Sri Rejeki
+ * KELAS    : IF2
+ * NIM      : 10119044
+ * Deskripsi Program : Program ini bertujuan menampilkan perkenalan diri mahasiswa 
+ * yang berbasis object oriented 
+ */
+
+public class Mahasiswa {
+    
+    public String nim;
+    public String nama;
+    
+    public void perkenalanDiri(){
+        System.out.println("Hallo Everyone");
+        System.out.println("My NIM is "+nim);
+        System.out.println("My Name is "+nama);
+        System.out.println();
+    }  
+}
